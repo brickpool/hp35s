6bbc40002db40f64e38b5e6a1c2d77dc920e83e8
brickpool <brickpool@web.de>
2019-05-14 14:57:25 +0200

    Add file

---
+++ src/generate.cmd
@@ -0,0 +1,5 @@
+asm2hpc.pl --shortcut --unicode --file=C/C.asm > C/C.35s
+asm2hpc.pl --shortcut --unicode --file=J/J.asm > J/J.35s
+asm2hpc.pl --shortcut --unicode --file=O/O.asm > O/O.35s
+asm2hpc.pl --shortcut --unicode --file=P/P.asm > P/P.35s
+asm2hpc.pl --shortcut --unicode --file=Y/Y.asm > Y/Y.35s
