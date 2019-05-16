cf4538a09ccdfb7776a458fce44ce41ea1d994f6
brickpool <brickpool@web.de>
2019-05-16 08:21:07 +0200

    Add new output format

--- src/generate.cmd
+++ src/generate.cmd
@@ -1,3 +1,18 @@
+rem Output with HP character set
+asm2hpc.pl --file=C/C.asm > C/C.txt
+asm2hpc.pl --file=J/J.asm > J/J.txt
+asm2hpc.pl --file=O/O.asm > O/O.txt
+asm2hpc.pl --file=P/P.asm > P/P.txt
+asm2hpc.pl --file=Y/Y.asm > Y/Y.txt
+
+rem Output as ASCII (7-bit) with shortcut keys as comment
+asm2hpc.pl --shortcut --ascii --file=C/C.asm > C/C.asc
+asm2hpc.pl --shortcut --ascii --file=J/J.asm > J/J.asc
+asm2hpc.pl --shortcut --ascii --file=O/O.asm > O/O.asc
+asm2hpc.pl --shortcut --ascii --file=P/P.asm > P/P.asc
+asm2hpc.pl --shortcut --ascii --file=Y/Y.asm > Y/Y.asc
+
+rem Output as Unicode (UTF-8) with shortcut keys
 asm2hpc.pl --shortcut --unicode --file=C/C.asm > C/C.35s
 asm2hpc.pl --shortcut --unicode --file=J/J.asm > J/J.35s
 asm2hpc.pl --shortcut --unicode --file=O/O.asm > O/O.35s
